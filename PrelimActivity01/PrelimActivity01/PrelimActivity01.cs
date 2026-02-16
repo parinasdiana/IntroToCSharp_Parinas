a723cc3ec680984831ed371b5c7d9065c1778b66
Diana <parinasdiana2005@gmail.com>
2026-02-16 09:27:35 +0800

    Update PrelimActivity01.cs

--- PrelimActivity01/PrelimActivity01/PrelimActivity01.cs
+++ PrelimActivity01/PrelimActivity01/PrelimActivity01.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace CodacLogistics
 {
@@ -9,10 +9,10 @@ namespace CodacLogistics
 			// --- TASK 1: Driver Profile & Distance Validation ---
 			Console.WriteLine("=== CODAC LOGISTICS FUEL AUDIT ===");
 			Console.Write("Enter Driver's Full Name: ");
-			string driverName = Console.ReadLine();
+			string driverName = Console.ReadLine(); // string used for textual data
 
 			Console.Write("Enter Weekly Fuel Budget: ");
-			decimal fuelBudget = decimal.Parse(Console.ReadLine());
+			decimal fuelBudget = decimal.Parse(Console.ReadLine()); // decimal used for financial accuracy
 
 			double totalDistance = 0;
 			while (totalDistance < 1.0 || totalDistance > 5000.0)
@@ -27,26 +27,26 @@ namespace CodacLogistics
 			}
 
 			// --- TASK 2: Fuel Expense Tracking ---
-			decimal[] fuelExpenses = new decimal[5];
+			decimal[] fuelExpenses = new decimal[5]; // 1D Array to hold 5 days of fuel data
 			decimal totalFuelSpent = 0;
 
 			for (int i = 0; i < 5; i++)
 			{
 				Console.Write($"Enter fuel cost for Day {i + 1}: ");
 				fuelExpenses[i] = decimal.Parse(Console.ReadLine());
-				totalFuelSpent += fuelExpenses[i];
+				totalFuelSpent += fuelExpenses[i]; // Accumulate total
 			}
 
 			// --- TASK 3: Performance Analysis --
-			decimal averageDailyExpense = totalFuelSpent / 5;
-			double efficiency = totalDistance / (double)totalFuelSpent;
+			decimal averageDailyExpense = totalFuelSpent / 5; // Calculate average
+			double efficiency = totalDistance / (double)totalFuelSpent; // Cast to double for calculation
 			string rating = "";
 
 			if (efficiency > 15) { rating = "High Efficiency"; }
 			else if (efficiency >= 10) { rating = "Standard Efficiency"; }
 			else { rating = "Low Efficiency / Maintenance Required"; }
 
-			bool isUnderBudget = totalFuelSpent <= fuelBudget;
+			bool isUnderBudget = totalFuelSpent <= fuelBudget; // Boolean check for budget status
 
 			// --- TASK 4: The Audit Report ---
 			Console.WriteLine("\n--- AUDIT REPORT ---");
